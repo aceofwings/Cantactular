a2ced7a752c033cef1a1da1fb246b99f0895f86a
cow <cow@cows-MacBook-Pro.local>
2017-01-27 16:53:59 -0500

    Add Mapping methods to ObjectDictionary

--- src/objectdictionary.py
+++ src/objectdictionary.py
@@ -10,19 +10,28 @@ class ObjectDictionary(collections.Mapping):
         pass
 
     def __setitem__(self,key,value):
-        pass
+        if type(key) is str:
+            self.names[key] = value
+        else:
+            self.ids[key] = value
 
     def __getitem__(self,key):
-        pass
+        if type(key) is str:
+            return self.names[key]
+        else:
+            return self.ids[key]
 
-    def __iter__():
-        pass
 
-    def __len__():
-        pass
+    def __iter__(self):
+        for objitem in self.ids:
+            yield objitem
+
+
+    def __len__(self):
+        return len(self.ids)
 
 
 
 
 if __name__ == '__main__':
-   cow = ObjectDictionary()
+   test = ObjectDictionary()
