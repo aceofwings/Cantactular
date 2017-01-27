acade4ed40d3e7fe02b972287fcb1402ae6f17a9
cow <cow@cows-MacBook-Pro.local>
2017-01-27 16:26:13 -0500

    Outline ObjectDictionary

---
+++ src/objectdictionary.py
@@ -0,0 +1,28 @@
+import collections
+
+class ObjectDictionary(collections.Mapping):
+
+    def __init__(self):
+        self.names = {}
+        self.ids = {}
+    @classmethod
+    def initialize(edsPath):
+        pass
+
+    def __setitem__(self,key,value):
+        pass
+
+    def __getitem__(self,key):
+        pass
+
+    def __iter__():
+        pass
+
+    def __len__():
+        pass
+
+
+
+
+if __name__ == '__main__':
+   cow = ObjectDictionary()
