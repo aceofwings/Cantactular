e8b92ccf861944f61d7a9e74f483420783f07924
monaco18matt <monaco18matt@yahoo.com>
2017-01-28 19:47:19 -0500

    Updated record and values classes

--- src/objectdictionary.py
+++ src/objectdictionary.py
@@ -1,4 +1,4 @@
-import collections, configparser
+ import collections, configparser
 
 class DictObj():
     index = 0x0
@@ -9,6 +9,51 @@ class DictObj():
         self.index = index
         self.ParameterName = parameterName
         self.ObjectType = objectType
+
+class Record(DictObj):
+    SubNumber = 0
+    OBJECT = ''
+    SECTION = ''
+    CATEGORY = ''
+    Subs = []
+
+    def __init__(self, subnumber, obj, section, category):
+        DictObj.__init__(self, index, parameterName, 9)
+        self.SubNumber = subnumber
+        self.OBJECT = obj
+        self.SECTION = section
+        self.CATEGORY = category
+
+        # imcomplete/ not accurate 
+        for sub in SubNumber:
+            Subs += []
+
+
+
+
+class Value(DictObj):
+    DataType =0
+    AccessType = ''
+    LowLimit = 0
+    HighLimit = 0 #might be hex value
+    PDOMapping = 0
+    DefaultValue = 0
+    OBJECT_VERSION = 0 
+    ACCESS_LEVEL = 0
+    SCALING = 0
+    EEPROM = ''
+
+    def __init__(self, datatype, accesstype, lowlimit, highlimit, pdomapping, defaultvalue, object_verison, access_level, scaling, eeprom):
+        self.DataType = datatype
+        self.AccessType = accesstype
+        self.LowLimit = lowlimit
+        self.HighLimit = highlimit
+        self.PDOMapping = pdomapping
+        self.DefaultValue = defaultvalue
+        self.OBJECT_VERSION = object_verison 
+        self.ACCESS_LEVEL = access_level
+        self.SCALING = scaling
+        self.EEPROM = eeprom
 
 class ObjectDictionary(collections.Mapping):
 
