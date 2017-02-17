1ac8cef7746fa6ef3e9d1b3004867598fc7b83c9
cow <cow@cows-MBP.wireless.rit.edu>
2017-02-17 14:29:15 -0500

    Test Finder , add default Runner and Loader, change testing protocole refactor file names

---
+++ tests/CAN/tests.py
@@ -0,0 +1,12 @@
+#
+#NOTE by providing a load_tests() function will be called to help load anything
+#from the subdirectories.
+#
+
+
+#
+#Load test from subpackages
+#
+def load_tests(loader, tests, pattern):
+    suite = TestSuite()
+    return suite
