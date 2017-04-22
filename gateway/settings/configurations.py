ce2061530206e5023079260c7af177ea62ac3848
Daniel Harrington <dan303h@gmail.com>
2017-04-22 03:14:19 -0400

    Edit .gitignore

--- gateway/settings/configurations.py
+++ gateway/settings/configurations.py
@@ -4,12 +4,12 @@ from gateway.evtcan.controller import EvtCanController
 
 class Configuration(object):
     #define the interfaces and their base controller types
-    interfaceNames = {'vcan0' : CanOpenController}
+    interfaceNames = {'vcan0' : EvtCanController}
     #silence any error on loading user customizable controllers
 
     maindbcFile = "INTEL_EVT_CAN.dbc"
 
-    failureSilence = True
+    failureSilence = False
     #force start the interfaces at the earliest moment
     forceStartInterfaces = False
 
