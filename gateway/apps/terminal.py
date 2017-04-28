dfebfd722596f09e4be84226a19692b2d6c07e0a
Daniel Harrington <dan303h@gmail.com>
2017-04-28 15:44:28 -0400

    Properly load the termopen controller

--- gateway/apps/terminal.py
+++ gateway/apps/terminal.py
@@ -13,31 +13,13 @@ class Terminal(object):
     def __init__(self):
         super()
         term = TermEvtCanController("INTEL_EVT_CAN.dbc")
+        canopen = TermOpenController()
+
         #term = TermOpenController()
         buildController(term)
-        self.screen = curses.initscr()
-        self.screen.keypad(1)
-        signal.signal(signal.SIGINT, self.close)
-        number = 0
-        curses.noecho()
-
-        self.temps = {}
-        term.controllerListener.addHandler(0x80,self.update)
-        term.controllerListener.addHandler(0x81,self.update)
-        term.controllerListener.addHandler(0x100,self.update)
-        term.controllerListener.addHandler(0x101,self.update)
-        term.controllerListener.addHandler(0x180,self.update)
-        term.controllerListener.addHandler(0x181,self.update)
-        term.controllerListener.addHandler(0x200,self.update)
-        term.controllerListener.addHandler(0x201,self.update)
-        while 1:
-            n = 0
-            for key in self.temps.keys():
-                for x in range(0, len(self.temps[key].signals)):
-                    self.screen.addstr(n, x*2+5, "Temps: " + str(self.temps[key]))
-                n+=1
-
-            self.screen.refresh()
+        buildController(canopen)
+        while True:
+            pass
 
         #term.motor.sdo.write_values[0x2220] = number
 
