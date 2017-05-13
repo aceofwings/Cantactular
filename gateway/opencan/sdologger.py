17fbbaa954128d622cf9e110cf0112f720de0995
Nicholas <nickama777@gmail.com>
2017-05-13 16:47:41 -0400

    Added Calculated MPH from rpm

--- gateway/opencan/sdologger.py
+++ gateway/opencan/sdologger.py
@@ -33,6 +33,8 @@ class SDOLog(object):
         logger.debug(log)
         pname = self.sdo.objectDictionary[message.index].parametername
         self.sdo.device.values[pname] = int(message.hexstring, 16)
+        if(pname is 'Velocity') :
+            self.sdo.device.values['Calculated MPH'] = int(message.hexstring, 16) * 100 / 16896
 
         self.sdo.read(self.readhandle, message.index, message.sub)
 
