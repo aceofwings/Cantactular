d2f12673406df046c42e58548152386a4785f1e2
Daniel Harrington <dan303H@gmail.com>
2017-09-13 00:00:20 -0400

    Add setup tools and installer

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+
+setup(name='evtgateway',
+      version='0.2',
+      description='Gateway for RIT REV1 Sport Bike',
+      url='https://github.com/aceofwings/Evt-Gateway',
+      author='EVT RIT',
+      author_email='EVT@RIT.EDU',
+      packages=find_packages(),
+      scripts= ['bin/gateway', 'bin/cansetup'],
+      license='MIT',
+      include_package_data=False,
+      package_data={ '': ['*.txt', '*.rst']},
+      zip_safe=False)
+
+
+BIN_PATH = '/usr/local/bin'
