5de5c565bbe81523bf23b05c4313c26e269abad8
DANIEL HARRINGTON <dan303h@gmail.com>
2017-09-20 20:35:03 -0400

    Remove module level code. Refactor defintions to be in there correct places.

--- gateway/command.py
+++ gateway/command.py
@@ -7,19 +7,20 @@ from . import commands
 from  importlib import import_module
 
 #Package where commands are located
-PACKAGE = "gateway.commands"
+def buildCommandParser():
+    DESCRIPTION = "Collects and Recieves and Analyzes Data"
+    parser = argparse.ArgumentParser(prog='Gateway',description=DESCRIPTION, add_help=False)
+    parser.PACKAGE = "gateway.commands"
+    parser.add_argument('--commands', action='help', default=argparse.SUPPRESS,
+                       help='list the commands available')
+    parser.add_argument('--version', action='version', version='%(prog)s ' + gateway.__version__)
+    parser.listOfCommands = {}
 
-DESCRIPTION = "Collects and Recieves and Analyzes Data"
+    modules = pkgutil.iter_modules([os.path.dirname(commands.__file__)])
+    for module in modules:
+        parser.listOfCommands[module[1]] = import_module('.' + module[1], parser.PACKAGE)
 
-parser = argparse.ArgumentParser(prog='Gateway',description=DESCRIPTION, add_help=False)
-
-parser.add_argument('--commands', action='help', default=argparse.SUPPRESS,
-                   help='list the commands available')
-
-parser.add_argument('--version', action='version', version='%(prog)s ' + gateway.__version__)
-
-listOfCommands = {}
-
+    return parser
 
 
 def gatewayCommandLine():
@@ -28,34 +29,27 @@ def gatewayCommandLine():
     After loading all modules it will take the first argument and match it with
     a namspace.
     '''
-    modules = pkgutil.iter_modules([os.path.dirname(commands.__file__)])
-    for module in modules:
-        listOfCommands[module[1]] = import_module('.' + module[1], PACKAGE)
+    parser = buildCommandParser()
+    parser.add_argument('service', choices=parser.listOfCommands.keys())
 
-    parser.add_argument('service', choices=listOfCommands.keys())
+    arg, options = parser.parse_known_args()
+    commandNamespace = parser.listOfCommands[arg.service]
 
     try:
-        executeCommand()
+        executeCommand(arg,options,commandNamespace)
     except ClassNonExistent as e:
         print(str(e) + " " + e.errors)
 
+def executeCommand(arg,options,namespace):
 
-
-def executeCommand():
-    arg, options = parser.parse_known_args()
-    if arg.service not in listOfCommands:
-        parser.print_help()
-        parser.exit()
-
-    commandNamespace = listOfCommands[arg.service]
-    members = inspect.getmembers(commandNamespace, inspect.isclass)
+    members = inspect.getmembers(namespace, inspect.isclass)
     #Attempt to find the class definition
     commandDefiniton = list(filter(lambda name: name[0].upper() == arg.service.upper() + "COMMAND" , members))
 
     if not commandDefiniton:
         raise ClassNonExistent("Could not find the command class", arg.service.capitalize() + "Command")
 
-    command = getattr(commandNamespace, commandDefiniton[0][0])
+    command = getattr(namespace, commandDefiniton[0][0])
     command(options).execute()
 
 class ClassNonExistent(Exception):
