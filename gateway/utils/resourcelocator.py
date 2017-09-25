4a6fab18d247042e70235ca3410fd873b00419f9
DANIEL HARRINGTON <dan303h@gmail.com>
2017-09-25 17:02:11 -0400

    remove complexity default relative arg is empty string

--- gateway/utils/resourcelocator.py
+++ gateway/utils/resourcelocator.py
@@ -12,24 +12,19 @@ class ResourceLocator(object):
         super().__init__()
         self.ROOT_PATH = root_path
 
-    @staticmethod
-    def get_locator(cls,relative_path=None):
+    @classmethod
+    def get_locator(cls,relative_path=""):
         """
         get_locator - returns a resource locator within the project directory
         :param relative_path: specify a path relative to the projects ROOT_PATH
         """
         path = relative_path
-        locator = ResourceLocator(path)
         if cls.ROOT_PATH is None:
-            if relative_path is None:
-                raise NoPathSpecified("No ROOT_PATH or relative_path specified")
-        else:
-            path = os.join(cls.ROOT_PATH,relative_path)
-
+            raise NoPathSpecified("No ROOT_PATH specified")
+        path = os.path.join(cls.ROOT_PATH,relative_path)
         if not os.path.isdir(path):
             raise NonExistentDirectory("Folder non existent ", path)
         locator = ResourceLocator(path)
-
         return locator
 
 class NonExistentDirectory(Exception):
