cdfc92a4cc95d2607e08758899564e521885420c
Daniel Harrington <dan303H@gmail.com>
2017-11-16 09:48:39 -0500

    Merging errors

--- gateway/launcher.py
+++ gateway/launcher.py
@@ -1,33 +1,15 @@
-<<<<<<< HEAD
-#Use project paths as root path and configuration.
-#Do not import this file directory
 
-=======
 """
 Launcher.py
 Responsible for mapping out project resources and intializing any other
 shared resources used between commands.
 
 """
->>>>>>> rev2devd
 #Preconfiguration Code goes here
 #Do not put code for specific applications here.
 #Shared resource code would benefit if done in the launcher.
 #
 import os
-<<<<<<< HEAD
-from gateway.utils.projectpaths import ProjectPath
-from gateway.settings.loader  import loadLogger
-
-#setup the project path
-ROOT_PATH = os.path.abspath('.')
-ProjectPath(ROOT_PATH)
-loadLogger()
-
-#import commands
-import gateway.core.engine
-from . import commands
-=======
 import logging
 from gateway.command import gatewayCommandLine
 from gateway.utils.resourcelocator import ResourceLocator
@@ -35,4 +17,3 @@ from gateway.utils.resourcelocator import ResourceLocator
 ROOT_PATH = os.path.abspath('.')
 ResourceLocator.ROOT_PATH = ROOT_PATH
 gatewayCommandLine()
->>>>>>> rev2devd
